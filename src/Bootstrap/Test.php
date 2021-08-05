a3482ce978f2e0a3756122562c1221ae15851386
张启全 <admin@zhanshop.cn>
2021-08-05 10:37:04 +0800

    1

---
+++ src/Bootstrap/Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author zhangqiquan
+ * @time 2021/8/5 10:30 上午
+ * @email: 768617998@qq.com
+ */
+namespace qiya\Bootstrap;
+class Test
+{
+    public static function test(){
+        echo 12;
+    }
+}
